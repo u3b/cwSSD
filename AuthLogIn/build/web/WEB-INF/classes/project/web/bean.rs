5c54821d99b211cd6d9d387987413c164b92bbec
ryan <R.Parr@2016.ljmu.ac.uk>
2018-04-03 21:19:56 +0100

    cwSSD | v.0.2
    Overdue Commit v.0.2
    
    Change log:
    
    Added In Full Login System using database
    Add & Remove User from database
    Privilege levels on the system, user and admin.
    Different web pages for users and admins
    
    Current Vulnerabilities:
    
    XSS Through Registration form, executing at index login.
    SQLi through registration, can be done to add a admin account.
    You can bypass authentication, without logging in if you know the exact URL, no active filtering done to prevent this.

---
+++ AuthLogIn/build/web/WEB-INF/classes/project/web/bean.rs
@@ -0,0 +1,2 @@
+login_bean
+bean
--- AuthLogIn/src/java/project/web/bean.java
+++ AuthLogIn/src/java/project/web/bean.java
@@ -1,49 +1,170 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package project.web;
 
-import javax.inject.Named;
-import java.io.Serializable;
-
-/**
- *
- * @author cmpkmcdo
- */
-@Named(value = "bean")
-public class bean implements Serializable {
-
-    private String name;
-    private String pass;
-    public boolean isLogged;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import java.sql.*;
+import java.util.*;
+import java.sql.PreparedStatement;
+ 
+@ManagedBean(name="bean")
+@RequestScoped
+public class bean {
+    private String username;
+    private String password;
+    private String databaseUsername;
+ 
+    private String databasePassword;
+    Connection connect;
+    PreparedStatement prepareStatement;
+    Statement preStatement;
+    ResultSet resultSet;
+    String SQL_Str;
+    int i=0;
+    private String userLevel;
+    private String databaseLevel;
+ 
+    public void databaseData(String User)
+    {
+        try
+        {
+            Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+            connect = DriverManager.getConnection("jdbc:derby://localhost:1527/policeDatabase","root","root");
+            SQL_Str="Select * from USER_DETAIL where USERNAME ='" + User +"'";
+            preStatement = connect.createStatement();
+            resultSet=preStatement.executeQuery(SQL_Str);
+            resultSet.next();
+            databaseUsername=resultSet.getString(1).toString();
+            databasePassword=resultSet.getString(2).toString();
+            databaseLevel=resultSet.getString(3).toString();
+            
+        }
+        
+        
+        catch(Exception ex)
+        {
+            ex.printStackTrace();
+            System.out.println("Exception" + ex);
+        }
+        
+    }
+    public void databaseDataInsertAdmin()
+    {
+        try
+        {
+            Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+            connect = DriverManager.getConnection("jdbc:derby://localhost:1527/policeDatabase","root","root");
+        
+            /*SQL_Str="INSERT INTO USER_DETAIL (USERNAME, PASSWORD) VALUES (?,?);";
+            preStatement = connect.prepareStatement(SQL_Str);
+            prepareStatement.setString(1, username);
+            prepareStatement.setString(2, password);
+            i = prepareStatement.executeUpdate();*/
+            
+            preStatement = connect.createStatement();
+            SQL_Str="INSERT INTO USER_DETAIL(USERNAME, PASSWORD, user_level) VALUES('" + username +"','" + password +"','admin')";
+            preStatement.executeUpdate(SQL_Str);
+            System.out.println("Success");
+        }
+        catch(Exception ex)
+        {
+            ex.printStackTrace();
+            System.out.println("Exception" + ex);
+        }
+    }
+    public void databaseDataInsertUser()
+    {
+        try
+        {
+            Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+            connect = DriverManager.getConnection("jdbc:derby://localhost:1527/policeDatabase","root","root");
+        
+            /*SQL_Str="INSERT INTO USER_DETAIL (USERNAME, PASSWORD) VALUES (?,?);";
+            preStatement = connect.prepareStatement(SQL_Str);
+            prepareStatement.setString(1, username);
+            prepareStatement.setString(2, password);
+            i = prepareStatement.executeUpdate();*/
+            
+            preStatement = connect.createStatement();
+            SQL_Str="INSERT INTO USER_DETAIL(USERNAME, PASSWORD, user_level) VALUES('" + username +"','" + password +"','user')";
+            preStatement.executeUpdate(SQL_Str);
+            System.out.println("Success");
+        }
+        catch(Exception ex)
+        {
+            ex.printStackTrace();
+            System.out.println("Exception" + ex);
+        }
+    }
     
-    
-    /**
-     * Creates a new instance of bean
-     */
-    public bean() {
+    public void databaseDataRemove()
+    {
+        try
+        {
+            Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+            connect = DriverManager.getConnection("jdbc:derby://localhost:1527/policeDatabase","root","root");
+        
+            /*SQL_Str="INSERT INTO USER_DETAIL (USERNAME, PASSWORD) VALUES (?,?);";
+            preStatement = connect.prepareStatement(SQL_Str);
+            prepareStatement.setString(1, username);
+            prepareStatement.setString(2, password);
+            i = prepareStatement.executeUpdate();*/
+            
+            preStatement = connect.createStatement();
+            SQL_Str="DELETE FROM USER_DETAIL WHERE USERNAME =('" + username +"')";
+            
+            preStatement.executeUpdate(SQL_Str);
+            System.out.println("Success");
+        }
+        catch(Exception ex)
+        {
+            ex.printStackTrace();
+            System.out.println("Exception" + ex);
+        }
     }
-
-    public String getName() {
-        return name;
+    public String getPassword() {
+        return password;
     }
-
-    public void setName(String name) {
-        this.name = name;
+ 
+    public void setPassword(String password) {
+        this.password = password;
     }
-
-    public String getPass() {
-        return pass;
+ 
+    public String getUsername() {
+        return username;
     }
-
-    public void setPass(String pass) {
-        this.pass = pass;
+ 
+    public void setUsername(String username) {
+        this.username = username;
     }
-    
-    public String action() {
-        isLogged = true;
-        return "forum.xhtml?faces-redirect=true";
+    public String checkForValidUser()
+    {
+        databaseData(username);
+ 
+        if(databaseLevel.equals("user"))
+        {
+ 
+            if(password.equals(databasePassword))
+                return "1";
+            
+            else
+            {
+                return "0";
+            }
+        }
+        else if(databaseLevel.equals("admin"))
+        {
+ 
+            if(password.equals(databasePassword))
+                return "2";
+            
+            else
+            {
+                return "0";
+            }
+        }
+        else
+        {
+            return "0";
+        }
     }
 }
